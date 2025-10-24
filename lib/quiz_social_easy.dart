539f1a77f64b2703e5f46463dff1cfc48b9bfe5e
Rizal Andi <rizalsudjay05@gmail.com>
2025-10-24 11:02:42 +0700

    update

--- lib/quiz_social_easy.dart
+++ lib/quiz_social_easy.dart
@@ -1,5 +1,6 @@
 import 'dart:async'; // ✅ Tambahan: untuk fitur timer
 import 'package:flutter/material.dart'; // Mengimpor material design Flutter
+import 'soal_easy.dart'; // Mengimpor daftar soal dari file soal_easy.dart
 
 class QuizSocialEasyPage extends StatefulWidget {
   const QuizSocialEasyPage({Key? key}) : super(key: key);
@@ -93,163 +94,7 @@ class _QuizSocialEasyPageState extends State<QuizSocialEasyPage> {
     super.dispose();
   }
 
-  // ===== DAFTAR SOAL =====
-  final List<Map<String, dynamic>> questions = [
-    {
-      'question':
-          'Pada zaman Mesozoikum, salah satu periode yang ada pada era tersebut adalah …',
-      'answers': ['Paleogen', 'Cretaceous', 'Kambrium', 'Protozoikum'],
-      'correct': 'Cretaceous',
-    },
-    {
-      'question':
-          'Kumpulan bintang yang jumlahnya bermiliar-miliar merupakan pengertian singkat dari …',
-      'answers': ['Tata Surya', 'Planetoid', 'Asteroid', 'Galaksi'],
-      'correct': 'Galaksi',
-    },
-    {
-      'question': 'Lapisan atmosfer terbawah adalah …',
-      'answers': ['Troposfer', 'Mesosfer', 'Termosfer', 'Stratosfer'],
-      'correct': 'Troposfer',
-    },
-    {
-      'question': 'Penghasil kopi terbesar di dunia adalah …',
-      'answers': ['Brazil', 'Argentina', 'Chile', 'Colombia'],
-      'correct': 'Brazil',
-    },
-    {
-      'question': 'Salah satu penyebab runtuhnya Yugoslavia adalah …',
-      'answers': [
-        'Kematian Broz Tito',
-        'Invasi Serbia',
-        'Perang Dunia I',
-        'Menganut Sosialis',
-      ],
-      'correct': 'Kematian Broz Tito',
-    },
-    {
-      'question': 'Kerajaan Tarumanegara dipimpin oleh …',
-      'answers': ['Dedi Mulyadi', 'Mulawarman', 'Purnawarman', 'Sima'],
-      'correct': 'Purnawarman',
-    },
-    {
-      'question': 'Sultan Malik Al-Saleh pernah memimpin kerajaan …',
-      'answers': [
-        'Samudra Pasai',
-        'Darul Islam/TII',
-        'Kerajaan Aceh',
-        'Malaka',
-      ],
-      'correct': 'Samudra Pasai',
-    },
-    {
-      'question': 'Salah satu tokoh Reformasi Gereja adalah …',
-      'answers': [
-        'Leonardo Bonapartae',
-        'Bonmati',
-        'Michaelangelo Bunorrotti',
-        'Martin Luther',
-      ],
-      'correct': 'Martin Luther',
-    },
-    {
-      'question':
-          'Kerajaan yang berhasil menaklukkan Kota Konstantinopel pada tahun 1453 M adalah …',
-      'answers': [
-        'Romawi',
-        'Kerajaan Inggris',
-        'Ottoman (Utsmani)',
-        'Kerajaan Mughal',
-      ],
-      'correct': 'Ottoman (Utsmani)',
-    },
-    {
-      'question': 'Penemu mesin uap adalah …',
-      'answers': ['Abraham Darby', 'James Watt', 'Morse', 'Elias Howe'],
-      'correct': 'James Watt',
-    },
-    {
-      'question':
-          'Perubahan cara produksi barang dari manusia ke mesin disebut juga …',
-      'answers': [
-        'Revolusi Perancis',
-        'Revolusi Industri',
-        'Renaissance',
-        'Europe Springs',
-      ],
-      'correct': 'Revolusi Industri',
-    },
-    {
-      'question': 'Salah satu tokoh Renaissance adalah …',
-      'answers': ['Leonardo Davinci', 'John Calvin', 'Nico Paz', 'Eren Yeager'],
-      'correct': 'Leonardo Davinci',
-    },
-    {
-      'question': 'Orang Belanda pertama yang tiba di Indonesia adalah …',
-      'answers': [
-        'Pieter Both',
-        'Leonardo Bonapartae',
-        'Jack Sparrow',
-        'Cornelius de Houtman',
-      ],
-      'correct': 'Cornelius de Houtman',
-    },
-    {
-      'question':
-          'Daerah Indonesia yang saat ini masih menganut sistem monarki adalah …',
-      'answers': ['Aceh Darussalam', 'Bandung', 'Los Santos', 'Yogyakarta'],
-      'correct': 'Yogyakarta',
-    },
-    {
-      'question': 'Pemimpin partai PKI adalah …',
-      'answers': ['D.N. Aidit', 'Musso', 'Bahlil Goblins', 'Semaun'],
-      'correct': 'Semaun',
-    },
-    {
-      'question': 'Penggagas utama Indische Partij adalah …',
-      'answers': [
-        'Soekarno',
-        'Moh. Hatta',
-        'Danudirja Setyabudi',
-        'Ki Hajar Dewantara',
-      ],
-      'correct': 'Danudirja Setyabudi',
-    },
-    {
-      'question':
-          'Henk Sneevliet membawa kelompok Social Democratische Partij ke Indonesia dengan paham …',
-      'answers': ['Sosialis', 'Ide Marx-Lenin', 'Demokratik', 'Monarki'],
-      'correct': 'Ide Marx-Lenin',
-    },
-    {
-      'question': 'Sebab khusus terjadinya Perang Dunia ke-1 adalah …',
-      'answers': [
-        'Ideologi',
-        'Kematian Franz Ferdinan',
-        'Revolusi Eropa',
-        'Megawati',
-      ],
-      'correct': 'Kematian Franz Ferdinan',
-    },
-    {
-      'question':
-          'Pemimpin reformis yang berperan besar dalam runtuhnya Uni Soviet adalah …',
-      'answers': [
-        'Vladimir Bahlil',
-        'Gibran',
-        'Mikhail Gorbachev',
-        'Benito Musollini',
-      ],
-      'correct': 'Mikhail Gorbachev',
-    },
-    {
-      'question':
-          'Blok Barat memiliki NATO, sebagai tandingan Uni Soviet membentuk …',
-      'answers': ['SEATO', 'AFC', 'Conmebol', 'Pakta Warsawa'],
-      'correct': 'Pakta Warsawa',
-    },
-  ];
-
+// animasi transisi soal dan jawaban
   void onCheckPressed() {
     if (selectedAnswer == null) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -366,7 +211,7 @@ class _QuizSocialEasyPageState extends State<QuizSocialEasyPage> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // ✅ Tambahan: pindahkan stage dan timer ke atas soal
+                  // Tambahan: pindahkan stage dan timer ke atas soal
                   Text(
                     'Stage ${currentQuestion + 1} / ${questions.length}',
                     style: const TextStyle(
---
+++ lib/soal_easy.dart
@@ -0,0 +1,140 @@
+//SOAL SOCIAL EASY
+
+final List<Map<String, dynamic>> questions = [
+  {
+    'question': 'Hewan yang mengalami metamorfosis sempurna adalah..',
+    'answers': ['Kucing', 'Ayam', 'Kupu-kupu', 'Ikan'],
+    'correct': 'Kupu-kupu',
+  },
+  {
+    'question': 'Pemimpin partai PKI adalah …',
+    'answers': ['D.N. Aidit', 'Musso', 'Bahlil Goblins', 'Semaun'],
+    'correct': 'Semaun',
+  },
+  {
+    'question':
+        'Kumpulan bintang yang jumlahnya bermiliar-miliar merupakan pengertian singkat dari …',
+    'answers': ['Tata Surya', 'Planetoid', 'Asteroid', 'Galaksi'],
+    'correct': 'Galaksi',
+  },
+  {
+    'question': 'Lapisan atmosfer terbawah adalah …',
+    'answers': ['Troposfer', 'Mesosfer', 'Termosfer', 'Stratosfer'],
+    'correct': 'Troposfer',
+  },
+  {
+    'question': 'Penghasil kopi terbesar di dunia adalah …',
+    'answers': ['Brazil', 'Argentina', 'Chile', 'Colombia'],
+    'correct': 'Brazil',
+  },
+  {
+    'question': 'Sumber energi terbesar bagi kehidupan di Bumi adalah..',
+    'answers': ['Matahari', 'Oksigen', 'Air', 'Batu bara'],
+    'correct': 'Matahari',
+  },
+  {
+    'question': 'Kerajaan Tarumanegara dipimpin oleh …',
+    'answers': ['Dedi Mulyadi', 'Mulawarman', 'Purnawarman', 'Sima'],
+    'correct': 'Purnawarman',
+  },
+  {
+    'question': 'Sultan Malik Al-Saleh pernah memimpin kerajaan …',
+    'answers': ['Samudra Pasai', 'Darul Islam/TII', 'Kerajaan Aceh', 'Malaka'],
+    'correct': 'Samudra Pasai',
+  },
+  {
+    'question':
+        'Partikel subatomik manakah yang bertanggung jawab atas ikatan kimia?',
+    'answers': ['Neutron', 'Proton', 'Elektron', 'Positron'],
+    'correct': 'Elektron',
+  },
+  {
+    'question':
+        'Kerajaan yang berhasil menaklukkan Kota Konstantinopel pada tahun 1453 M adalah …',
+    'answers': ['Romawi', 'Kerajaan Inggris', 'Ottoman', 'Kerajaan Mughal'],
+    'correct': 'Ottoman',
+  },
+  {
+    'question': 'Penemu mesin uap adalah …',
+    'answers': ['Abraham Darby', 'James Watt', 'Morse', 'Elias Howe'],
+    'correct': 'James Watt',
+  },
+  {
+    'question':
+        'Perubahan cara produksi barang dari manusia ke mesin disebut juga …',
+    'answers': [
+      'Revolusi Perancis',
+      'Revolusi Industri',
+      'Renaissance',
+      'Europe Springs',
+    ],
+    'correct': 'Revolusi Industri',
+  },
+  {
+    'question': 'Rangka manusia berfungsi untuk...',
+    'answers': [
+      'Bernafas',
+      'Melindungi organ dalam',
+      'Reproduksi',
+      'Mencerna makanan',
+    ],
+    'correct': 'Melindungi organ dalam',
+  },
+  {
+    'question': 'Orang Belanda pertama yang tiba di Indonesia adalah …',
+    'answers': [
+      'Pieter Both',
+      'Leonardo Bonapartae',
+      'Jack Sparrow',
+      'Cornelius de Houtman',
+    ],
+    'correct': 'Cornelius de Houtman',
+  },
+  {
+    'question':
+        'Daerah Indonesia yang saat ini masih menganut sistem monarki adalah …',
+    'answers': ['Aceh Darussalam', 'Bandung', 'Los Santos', 'Yogyakarta'],
+    'correct': 'Yogyakarta',
+  },
+  {
+    'question': 'Penggagas utama Indische Partij adalah …',
+    'answers': [
+      'Soekarno',
+      'Moh. Hatta',
+      'Danudirja Setyabudi',
+      'Ki Hajar Dewantara',
+    ],
+    'correct': 'Danudirja Setyabudi',
+  },
+  {
+    'question':
+        'Henk Sneevliet membawa kelompok Social Democratische Partij ke Indonesia dengan paham …',
+    'answers': ['Sosialis', 'Ide Marx-Lenin', 'Demokratik', 'Monarki'],
+    'correct': 'Ide Marx-Lenin',
+  },
+  {
+    'question': 'Sebab khusus terjadinya Perang Dunia ke-1 adalah …',
+    'answers': [
+      'Ideologi',
+      'Kematian Franz Ferdinan',
+      'Revolusi Eropa',
+      'Megawati',
+    ],
+    'correct': 'Kematian Franz Ferdinan',
+  },
+  {
+    'question': 'Berikut ini yang termasuk konduktor panas yang baik adalah..',
+    'answers': ['Emas', 'Karet', 'Plastik', 'Kayu'],
+    'correct': 'Emas',
+  },
+  {
+    'question':
+        'Blok Barat memiliki NATO, sebagai tandingan Uni Soviet membentuk …',
+    'answers': ['SEATO', 'AFC', 'Conmebol', 'Pakta Warsawa'],
+    'correct': 'Pakta Warsawa',
+  },
+];
+
+
+//SOAL SOCIAL HARD
+
